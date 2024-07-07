11bdeea8af10a7bee3b9a5d726a95f313d90b2e8
zainulla R <152606739+Zainulla1@users.noreply.github.com>
2024-07-07 20:06:17 +0530

    Add files via upload

---
+++ day27.js
@@ -0,0 +1,14 @@
+//Find the output of a Math.sqrt
+
+let num1 = Math.sqrt(-9);
+let num2 = Math.sqrt(9);
+
+console.log(num1 + num2);
+
+if (num1 + num2) {
+   console.log('Posititve Value');
+} else if (num1 + num2 >= 0) {
+    console.log('Negetive Value');
+} else {
+    console.log('Not number Value');
+}
